94ae82aa894c4346e29b1b11e66c050a10909b88
TERAS <TERAS>
2024-06-16 14:12:25 +0800

    20240616

---
+++ CustomMacroPluginGamepadMixer/MacroSample/Game_GamepadMixer.cs
@@ -0,0 +1,214 @@
+﻿using CustomMacroBase;
+using CustomMacroBase.CustomControlEx.RippleButtonEx;
+using CustomMacroBase.GamePadState;
+using CustomMacroBase.Helper;
+using CustomMacroBase.PreBase;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CustomMacroPluginGamepadMixer.MacroSample
+{
+    public partial class Game_GamepadMixer
+    {
+        private DS4StateLite[] gamepadList = new List<DS4StateLite>(Enumerable.Repeat(new DS4StateLite(), 2)).ToArray();
+        private ref DS4StateLite gamepad0 => ref gamepadList[0];
+        private ref DS4StateLite gamepad1 => ref gamepadList[1];
+    }
+
+    public partial class Game_GamepadMixer : MacroBase
+    {
+        public override void Init()
+        {
+            MainGate.Text = "Root";
+
+            MainGate.AddEx(() => CreateTextBlock("- To enable the mapping feature, set the Ex3 toggle switch to 'ON' position.", Colors.LightGray));
+            MainGate.AddEx(() => CreateTextBlock("- Be sure to use HidHide to solve the double input issue.", Colors.LightGray));
+
+            MainGate.Add(CreateGateBase("Options", true));   //[0]
+            {
+                MainGate[0].Add(CreateGateBase("Disable the output of the 2nd controller.", true));   //[0][0]
+                MainGate[0].Add(CreateGateBase("Only the 2nd controller can operate the buttons.", false));   //[0][1]
+            }
+
+            MainGate.Add(CreateGateBase("Managed by the 2nd Controller", true));   //[1]
+            {
+                MainGate[1].AddEx(() =>
+                {
+                    var sp = new StackPanel() { Orientation = Orientation.Horizontal };
+                    {
+                        sp.Children.Add(new cRippleButton()
+                        {
+                            Width = 80,
+                            Height = 22,
+                            Type = ButtonType.Custom,
+                            Text = "Enable All",
+                            Command = new RelayCommand(_ =>
+                            {
+                                MainGate[1].GateBaseList.ForEach(x => x.Enable = true);
+                            }),
+                            Margin = new(0, 0, 5, 0),
+                        });
+                        sp.Children.Add(new cRippleButton()
+                        {
+                            Width = 80,
+                            Height = 22,
+                            Type = ButtonType.Custom,
+                            Text = "Disable All",
+                            Command = new RelayCommand(_ =>
+                            {
+                                MainGate[1].GateBaseList.ForEach(x => x.Enable = false);
+                            }),
+                            Margin = new(0, 0, 5, 0),
+                        });
+                    }
+                    return sp;
+                });
+
+                MainGate[1].Add(CreateGateBase("□", true));   //[1][0]
+                MainGate[1].Add(CreateGateBase("△", true)); //[1][1]
+                MainGate[1].Add(CreateGateBase("○", true));   //[1][2]
+                MainGate[1].Add(CreateGateBase("×", true));    //[1][3]
+
+                MainGate[1].Add(CreateGateBase("DpadLeft", true));  //[1][4]
+                MainGate[1].Add(CreateGateBase("DpadUp", true));    //[1][5]
+                MainGate[1].Add(CreateGateBase("DpadRight", true)); //[1][6]
+                MainGate[1].Add(CreateGateBase("DpadDown", true));  //[1][7]
+
+                MainGate[1].Add(CreateGateBase("Share", true));       //[1][8]
+                MainGate[1].Add(CreateGateBase("Options", true));     //[1][9]
+                MainGate[1].Add(CreateGateBase("TouchButton", true)); //[1][10]
+
+                MainGate[1].Add(CreateGateBase("PS", true));   //[1][11]
+                MainGate[1].Add(CreateGateBase("Mute", true)); //[1][12]
+
+                MainGate[1].Add(CreateGateBase("L1", true)); //[1][13]
+                MainGate[1].Add(CreateGateBase("L2", true)); //[1][14]
+                MainGate[1].Add(CreateGateBase("L3", true)); //[1][15]
+
+                MainGate[1].Add(CreateGateBase("R1", true)); //[1][16]
+                MainGate[1].Add(CreateGateBase("R2", true)); //[1][17]
+                MainGate[1].Add(CreateGateBase("R3", true)); //[1][18]
+
+                MainGate[1].Add(CreateGateBase("Left Stick", true)); //[1][19]
+                MainGate[1].Add(CreateGateBase("Right Stick", true)); //[1][20]
+            }
+        }
+
+        public override void UpdateState()
+        {
+            if (MainGate.Enable is false) { return; }
+
+            Gamepad0(Ind, MainGate[1]);
+            Gamepad1(Ind);
+        }
+    }
+
+    public partial class Game_GamepadMixer
+    {
+        bool controller2_output_disabled => MainGate[0][0].Enable;
+        bool controller2_buttons_only => MainGate[0][1].Enable;
+
+        private void Gamepad0(int ind, GateBase host)
+        {
+            if (ind != 0) { return; }
+
+            //VirtualDS4.CopyTo(ref gamepadList[ind]);
+
+            // Map the button status of 2nd controller to 1st controller 
+            {
+                if (controller2_buttons_only)
+                {
+                    if (host[0].Enable) { VirtualDS4.Square = gamepad1.Square; }
+                    if (host[1].Enable) { VirtualDS4.Triangle = gamepad1.Triangle; }
+                    if (host[2].Enable) { VirtualDS4.Circle = gamepad1.Circle; }
+                    if (host[3].Enable) { VirtualDS4.Cross = gamepad1.Cross; }
+
+                    if (host[4].Enable) { VirtualDS4.DpadLeft = gamepad1.DpadLeft; }
+                    if (host[5].Enable) { VirtualDS4.DpadUp = gamepad1.DpadUp; }
+                    if (host[6].Enable) { VirtualDS4.DpadRight = gamepad1.DpadRight; }
+                    if (host[7].Enable) { VirtualDS4.DpadDown = gamepad1.DpadDown; }
+
+                    if (host[8].Enable) { VirtualDS4.Share = gamepad1.Share; }
+                    if (host[9].Enable) { VirtualDS4.Options = gamepad1.Options; }
+                    if (host[10].Enable) { VirtualDS4.OutputTouchButton = gamepad1.TouchButton; }
+
+                    if (host[11].Enable) { VirtualDS4.PS = gamepad1.PS; }
+                    if (host[12].Enable) { VirtualDS4.Mute = gamepad1.Mute; }
+
+                    if (host[13].Enable) { VirtualDS4.L1 = gamepad1.L1; }
+                    if (host[14].Enable) { VirtualDS4.L2 = gamepad1.L2; }
+                    if (host[15].Enable) { VirtualDS4.L3 = gamepad1.L3; }
+
+                    if (host[16].Enable) { VirtualDS4.R1 = gamepad1.R1; }
+                    if (host[17].Enable) { VirtualDS4.R2 = gamepad1.R2; }
+                    if (host[18].Enable) { VirtualDS4.R3 = gamepad1.R3; }
+
+                    //
+                    if (host[19].Enable) { VirtualDS4.LX = gamepad1.LX; VirtualDS4.LY = gamepad1.LY; }
+                    if (host[20].Enable) { VirtualDS4.RX = gamepad1.RX; VirtualDS4.RY = gamepad1.RY; }
+                }
+                else
+                {
+                    if (host[0].Enable) { VirtualDS4.Square |= gamepad1.Square; }
+                    if (host[1].Enable) { VirtualDS4.Triangle |= gamepad1.Triangle; }
+                    if (host[2].Enable) { VirtualDS4.Circle |= gamepad1.Circle; }
+                    if (host[3].Enable) { VirtualDS4.Cross |= gamepad1.Cross; }
+
+                    if (host[4].Enable) { VirtualDS4.DpadLeft |= gamepad1.DpadLeft; }
+                    if (host[5].Enable) { VirtualDS4.DpadUp |= gamepad1.DpadUp; }
+                    if (host[6].Enable) { VirtualDS4.DpadRight |= gamepad1.DpadRight; }
+                    if (host[7].Enable) { VirtualDS4.DpadDown |= gamepad1.DpadDown; }
+
+                    if (host[8].Enable) { VirtualDS4.Share |= gamepad1.Share; }
+                    if (host[9].Enable) { VirtualDS4.Options |= gamepad1.Options; }
+                    if (host[10].Enable) { VirtualDS4.OutputTouchButton |= gamepad1.TouchButton; }
+
+                    if (host[11].Enable) { VirtualDS4.PS |= gamepad1.PS; }
+                    if (host[12].Enable) { VirtualDS4.Mute |= gamepad1.Mute; }
+
+                    if (host[13].Enable) { VirtualDS4.L1 |= gamepad1.L1; }
+                    if (host[14].Enable) { VirtualDS4.L2 |= gamepad1.L2; }
+                    if (host[15].Enable) { VirtualDS4.L3 |= gamepad1.L3; }
+
+                    if (host[16].Enable) { VirtualDS4.R1 |= gamepad1.R1; }
+                    if (host[17].Enable) { VirtualDS4.R2 |= gamepad1.R2; }
+                    if (host[18].Enable) { VirtualDS4.R3 |= gamepad1.R3; }
+
+                    //
+                    if (host[19].Enable) { VirtualDS4.LX = AplusB(VirtualDS4.LX, gamepad1.LX); VirtualDS4.LY = AplusB(VirtualDS4.LY, gamepad1.LY); }
+                    if (host[20].Enable) { VirtualDS4.RX = AplusB(VirtualDS4.RX, gamepad1.RX); VirtualDS4.RY = AplusB(VirtualDS4.RY, gamepad1.RY); }
+                }
+            }
+        }
+
+        private void Gamepad1(int ind)
+        {
+            if (ind != 1) { return; }
+
+            VirtualDS4.CopyTo(ref gamepadList[ind]); //
+            if (controller2_output_disabled) { VirtualDS4.Reset(); }
+        }
+    }
+
+    public partial class Game_GamepadMixer
+    {
+        private TextBlock CreateTextBlock(string text, Color? color = null)
+        {
+            return new TextBlock()
+            {
+                Text = text,
+                Foreground = new SolidColorBrush(color ?? Colors.White),
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
+            };
+        }
+
+        private byte AplusB(byte a, byte b)
+        {
+            var va = a - 127;
+            var vb = b - 127;
+            var c = (va + vb);
+
+            return (byte)Math.Clamp((127 + c), byte.MinValue, byte.MaxValue);
+        }
+    }
+}
---
+++ CustomMacroPluginTouchPadMapper/MacroSample/Game_TouchPadMapper.cs
@@ -0,0 +1,281 @@
+﻿using CustomMacroBase;
+using CustomMacroBase.Helper;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace CustomMacroPluginGamepadMixer.MacroSample
+{
+    public partial class Game_TouchPadMapper
+    {
+        enum Position
+        {
+            TopLeft,
+            TopRight,
+            BottomLeft,
+            BottomRight,
+            Unknown
+        }
+
+        class InnerModel
+        {
+            public ObservableCollection<int> IntList = new(Enumerable.Repeat(0, 20));
+            public ObservableCollection<bool> BoolList = new(Enumerable.Repeat(false, 20));
+            public ObservableCollection<double> DoubleList = new(Enumerable.Repeat(0d, 20));
+        }
+
+        class InnerViewModel : NotificationObject
+        {
+            InnerModel model = new();
+
+            #region int
+            // 0~3
+            public int Touch0_X
+            {
+                get => model.IntList[0];
+                set
+                {
+                    if (model.IntList[0] != value)
+                    {
+                        model.IntList[0] = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+            public int Touch0_Y
+            {
+                get => model.IntList[1];
+                set
+                {
+                    if (model.IntList[1] != value)
+                    {
+                        model.IntList[1] = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+            public int Touch1_X
+            {
+                get => model.IntList[2];
+                set
+                {
+                    if (model.IntList[2] != value)
+                    {
+                        model.IntList[2] = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+            public int Touch1_Y
+            {
+                get => model.IntList[3];
+                set
+                {
+                    if (model.IntList[3] != value)
+                    {
+                        model.IntList[3] = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+
+            // 4~7
+            public int Center_X
+            {
+                get => model.IntList[4];
+                set
+                {
+                    if (model.IntList[4] != value)
+                    {
+                        model.IntList[4] = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+            public int Center_Y
+            {
+                get => model.IntList[5];
+                set
+                {
+                    if (model.IntList[5] != value)
+                    {
+                        model.IntList[5] = value;
+                        NotifyPropertyChanged();
+                    }
+                }
+            }
+            #endregion
+
+            #region bool
+            // 0
+
+            #endregion
+
+
+            public InnerViewModel()
+            {
+                Touch0_X = Touch0_Y = Touch1_X = Touch1_Y = -1;
+                Center_X = 959; Center_Y = 470;
+            }
+        }
+
+        static InnerViewModel viewmodel = new();
+    }
+
+    public partial class Game_TouchPadMapper : MacroBase
+    {
+        public override void Init()
+        {
+            MainGate.Text = "Root";
+
+            MainGate.AddEx(() =>
+            {
+                var sp = new StackPanel() { Orientation = Orientation.Horizontal };
+                {
+                    var sp0 = new StackPanel();
+                    {
+                        sp.Children.Add(CreateValueIndicator(viewmodel, [CreateValueIndicatorPacket(nameof(viewmodel.Touch0_X)),
+                                                                         CreateValueIndicatorPacket(nameof(viewmodel.Touch0_Y))]));
+                    }
+
+                    var sp1 = new StackPanel();
+                    {
+                        sp.Children.Add(CreateValueIndicator(viewmodel, [CreateValueIndicatorPacket(nameof(viewmodel.Touch1_X)),
+                                                                         CreateValueIndicatorPacket(nameof(viewmodel.Touch1_Y))]));
+                    }
+
+                    sp.Children.Add(sp0);
+                    sp.Children.Add(sp1);
+                }
+                return sp;
+            });
+            MainGate.AddEx(() => CreateSlider(0, 1919, viewmodel, nameof(viewmodel.Center_X), 1, sliderTextPrefix: $"Left", defalutValue: 959, sliderTextSuffix: "Right"));
+            MainGate.AddEx(() => CreateSlider(0, 941, viewmodel, nameof(viewmodel.Center_Y), 1, sliderTextPrefix: $"Top", defalutValue: 470, sliderTextSuffix: "Bottom"));
+
+            MainGate.Add(CreateGateBase("BottomLeft -> L3", true));   //[0]
+            MainGate.Add(CreateGateBase("BottomRight -> R3", true));
+            MainGate.Add(CreateGateBase("TopLeft -> L3 + R3", true));
+        }
+
+        public override void UpdateState()
+        {
+            if (MainGate.Enable is false) { return; }
+
+            GetTouchPos();
+
+            TouchPadBottomLeft(MainGate[0].Enable);
+            TouchPadBottomRight(MainGate[1].Enable);
+            TouchPadTopLeft(MainGate[2].Enable);
+        }
+    }
+
+    // MACRO
+    public partial class Game_TouchPadMapper
+    {
+        Func<Position> GetTouch0Pos = () => CalculatePos(viewmodel.Touch0_X, viewmodel.Touch0_Y, viewmodel.Center_X, viewmodel.Center_Y);
+        Func<Position> GetTouch1Pos = () => CalculatePos(viewmodel.Touch1_X, viewmodel.Touch1_Y, viewmodel.Center_X, viewmodel.Center_Y);
+
+        private void GetTouchPos()
+        {
+            if (RealDS4.Touch0IsActive)
+            {
+                viewmodel.Touch0_X = RealDS4.Touch0[0];
+                viewmodel.Touch0_Y = RealDS4.Touch0[1];
+            }
+            else
+            {
+                viewmodel.Touch0_X = viewmodel.Touch0_Y = -1;
+            }
+
+            if (RealDS4.Touch1IsActive)
+            {
+                viewmodel.Touch1_X = RealDS4.Touch1[0];
+                viewmodel.Touch1_Y = RealDS4.Touch1[1];
+            }
+            else
+            {
+                viewmodel.Touch1_X = viewmodel.Touch1_Y = -1;
+            }
+        }
+
+        private void TouchPadBottomLeft(bool canexecute)
+        {
+            if (!canexecute) { return; }
+
+            var a = RealDS4.Touch0IsActive && GetTouch0Pos.Invoke() is Position.BottomLeft;
+            var b = RealDS4.Touch1IsActive && GetTouch1Pos.Invoke() is Position.BottomLeft;
+
+            if (a || b)
+            {
+                VirtualDS4.L3 = true;
+            }
+        }
+
+        private void TouchPadBottomRight(bool canexecute)
+        {
+            if (!canexecute) { return; }
+
+            var a = RealDS4.Touch0IsActive && GetTouch0Pos.Invoke() is Position.BottomRight;
+            var b = RealDS4.Touch1IsActive && GetTouch1Pos.Invoke() is Position.BottomRight;
+
+            if (a || b)
+            {
+                VirtualDS4.R3 = true;
+            }
+        }
+
+        private void TouchPadTopLeft(bool canexecute)
+        {
+            if (!canexecute) { return; }
+
+            var a = RealDS4.Touch0IsActive && GetTouch0Pos.Invoke() is Position.TopLeft;
+            var b = RealDS4.Touch1IsActive && GetTouch1Pos.Invoke() is Position.TopLeft;
+
+            if (a || b)
+            {
+                VirtualDS4.L3 = VirtualDS4.R3 = true;
+            }
+        }
+    }
+
+    // HELPER
+    public partial class Game_TouchPadMapper
+    {
+        private TextBlock CreateTextBlock(string text, Color? color = null)
+        {
+            return new TextBlock()
+            {
+                Text = text,
+                Foreground = new SolidColorBrush(color ?? Colors.White),
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
+            };
+        }
+        private ValueIndicatorPacket CreateValueIndicatorPacket(string name)
+        {
+            return new ValueIndicatorPacket(propname: name,
+                                            propnamecolor: new(Colors.Gray),
+                                            propvaluecolor: new(Colors.LawnGreen),
+                                            propvaluecolorswitcher: value =>
+                                            {
+                                                return value switch
+                                                {
+                                                    > 0 => new(Colors.LawnGreen),
+                                                    _ => new(Colors.Crimson)
+                                                };
+                                            });
+        }
+
+        private static Position CalculatePos(int x, int y, int cx, int cy)
+        {
+            var isTop = y <= cy;
+            var isLeft = x <= cx;
+
+            if (isTop && isLeft) { return Position.TopLeft; }
+            if (!isTop && isLeft) { return Position.BottomLeft; }
+            if (isTop && !isLeft) { return Position.TopRight; }
+            if (!isTop && !isLeft) { return Position.BottomRight; }
+
+            return Position.Unknown;
+        }
+    }
+}
